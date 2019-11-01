52bfcc0479677661f2c0fed67ba18c7f7a11d87c
dominik2 <dominik.nowocien@gmail.com>
2019-11-01 16:35:19 +0100

    optymalizacja

--- Dominik_gr5_projJA/Dominik_gr5_projJA/ImageProcessor.cs
+++ Dominik_gr5_projJA/Dominik_gr5_projJA/ImageProcessor.cs
@@ -58,8 +58,8 @@ namespace Dominik_gr5_projJA
         public void processingMethod(Object im)
         {
             Bitmap image = (Bitmap)im;
-            for (int x = 0; x < image.Width; x++)
-                for (int y = 0; y < image.Height; y++)
+            for (int y = 0; y < image.Height; y++)
+                for (int x = 0; x < image.Width; x++)
                 {
                     Color color = ToGrayScaleCSharp.Process(image.GetPixel(x, y));
 
