66f2c84f8e707ad598ebba6f71823cf3ab8cd48d
dominik2 <dominik.nowocien@gmail.com>
2019-11-01 16:14:43 +0100

    dodanie klasy zamieniajacej kolor na odcien szarości

---
+++ Dominik_gr5_projJA/Dominik_gr5_projJA/ToGrayScaleCSharp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Dominik_gr5_projJA
+{
+    static public class ToGrayScaleCSharp
+    {
+        public static Color Process(Color color)
+        {
+            int grayScaleColor = (color.R + color.G + color.B) / 3;
+            return Color.FromArgb(
+                grayScaleColor,
+                grayScaleColor,
+                grayScaleColor);
+        }
+
+    }
+}
