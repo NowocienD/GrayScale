ca797d47ba84fb1331c274a21c87542d61762167
dominik2 <dominik.nowocien@gmail.com>
2019-11-01 13:47:21 +0100

    dodana klasa ImageProcessor

---
+++ Dominik_gr5_projJA/Dominik_gr5_projJA/ImageProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominik_gr5_projJA
+{
+    class ImageProcessor
+    {
+    }
+}
