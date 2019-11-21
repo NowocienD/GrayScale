d52f3de722c2ce06999b75bf7ef1a78751a96977
Dominik Nowocien <dominik.nowocien@gmail.com>
2019-11-21 21:20:54 +0100

    aktualizacja sciezki biblioteki cpp

--- Zrodla/MAIN/Dominik_gr5_projJA/CppDll.cs
+++ Zrodla/MAIN/Dominik_gr5_projJA/CppDll.cs
@@ -10,7 +10,7 @@ namespace ColorToGrayScale
 {
     public class CppDll : ICppDll
     {
-        [DllImport(@"C:\Users\Dominik\Desktop\repos\asembler-\Zrodla\DLL_C\x64\Release\C_DLL.dll")]
+        [DllImport(@"C:\Users\qwertyuiop\Desktop\repo\Zrodla\DLL_C\x64\Release\C_DLL.dll")]
         public static extern int ColorChange(int r, int g, int b);
 
         public void ChangeColorToGrayScale(Bitmap image)
