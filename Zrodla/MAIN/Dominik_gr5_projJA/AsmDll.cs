b37723377aa0af526e0c7305c9c19e421c28b79b
Dominik Nowocien <dominik.nowocien@gmail.com>
2019-11-24 20:49:15 +0100

    a

--- Zrodla/MAIN/Dominik_gr5_projJA/AsmDll.cs
+++ Zrodla/MAIN/Dominik_gr5_projJA/AsmDll.cs
@@ -10,18 +10,54 @@ namespace ColorToGrayScale
 {
     public class AsmDll : IAsmDll
     {
-        [DllImport(@"C:\Users\Dominik\source\repos\ASM_DLL\x64\Release\ASM_DLL.dll")]
-        public static extern int ColorChange(int r, int g, int b);
+        [DllImport(@"C:\Users\qwertyuiop\source\repos\asm_dll_2017\x64\Release\asm_dll_2017.dll")]
+        public static extern int ColorChange(byte[] r, byte[] g, byte[] b);
 
-        public void ChangeColorToGrayScale(Bitmap image)
+        public void ChangeColorToGrayScale2(Bitmap image)
         {
             for (int x = 0; x < image.Width; x++)
             {
                 for (int y = 0; y < image.Height; y++)
                 {
-                    Color oldColor = image.GetPixel(x, y);
-                    int newColor = ColorChange(oldColor.R, oldColor.G, oldColor.B);
-                    image.SetPixel(x, y, Color.FromArgb(newColor, newColor, newColor));
+                    //Color oldColor = image.GetPixel(x, y);
+                    //int newColor = ColorChange(oldColor.R, oldColor.G, oldColor.B);
+                    //image.SetPixel(x, y, Color.FromArgb(newColor, newColor, newColor));
+                }
+            }
+        }
+
+        public void ChangeColorToGrayScale(Bitmap image)
+        {
+            const int size = 16;
+            //Bitmap image = (Bitmap)img;
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height - 16; y += size)
+                {
+                    byte[] newColorArrayRed = new byte[size];
+                    byte[] newColorArrayGreen = new byte[size];
+                    byte[] newColorArrayBlue = new byte[size];
+                    for (int i = 0; i < size; i++)
+                    {
+                        Color oldColor = image.GetPixel(x, y + i);
+                        newColorArrayRed[i] = oldColor.R;
+                        newColorArrayGreen[i] = oldColor.G;
+                        newColorArrayBlue[i] = oldColor.B;
+                    }
+
+                    ColorChange(newColorArrayRed, newColorArrayGreen, newColorArrayBlue);
+
+                    try
+                    {
+                        for (int i = 0; i < size; i++)
+                        {
+                            image.SetPixel(x, y + i, Color.FromArgb(newColorArrayRed[i], newColorArrayGreen[i], newColorArrayBlue[i]));
+                        }
+                    }
+                    catch 
+                    { 
+                    }
+                    //image.SetPixel(x, y, Color.FromArgb(newColor, newColor, newColor));
                 }
             }
         }
--- Zrodla/MAIN/Dominik_gr5_projJA/CppDll.cs
+++ Zrodla/MAIN/Dominik_gr5_projJA/CppDll.cs
@@ -11,7 +11,7 @@ namespace ColorToGrayScale
     public class CppDll : ICppDll
     {
         [DllImport(@"C:\Users\qwertyuiop\Desktop\repo\Zrodla\DLL_C\x64\Release\C_DLL.dll")]
-        public static extern int ColorChange(int r, int g, int b);
+        public static extern int ColorChange(byte[] r, byte[] g, byte[] b);
 
         public void ChangeColorToGrayScale(Bitmap image)
         {
@@ -19,9 +19,39 @@ namespace ColorToGrayScale
             {
                 for (int y = 0; y < image.Height; y++)
                 {
-                    Color oldColor = image.GetPixel(x, y);
-                    int newColor = ColorChange(oldColor.R, oldColor.G, oldColor.B);
-                    image.SetPixel(x, y, Color.FromArgb(newColor, newColor, newColor));
+                    //Color oldColor = image.GetPixel(x, y);
+                    //int newColor = ColorChange(oldColor.R, oldColor.G, oldColor.B);
+                    //image.SetPixel(x, y, Color.FromArgb(newColor, newColor, newColor));
+                }
+            }
+        }
+
+        public void SingleColorChannel(Object img)
+        {
+            Bitmap image = (Bitmap)img;
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y += 8)
+                {
+                    byte[] newColorArrayRed = new byte[8];
+                    byte[] newColorArrayGreen = new byte[8];
+                    byte[] newColorArrayBlue = new byte[8];
+                    for (int i = 0; i < 8; i++)
+                    {
+                        Color oldColor = image.GetPixel(x, y);
+                        newColorArrayRed[0] = oldColor.R;
+                        newColorArrayGreen[0] = oldColor.G;
+                        newColorArrayBlue[0] = oldColor.B;
+                    }
+
+                    ColorChange(newColorArrayRed, newColorArrayGreen, newColorArrayBlue);
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        image.SetPixel(x, y + i, Color.FromArgb(newColorArrayRed[i], newColorArrayGreen[i], newColorArrayBlue[i]));
+                    }
+
+                    //image.SetPixel(x, y, Color.FromArgb(newColor, newColor, newColor));
                 }
             }
         }
