3ffe93c3205be9cff5a10c47da9bc38f71f84175
Dominik Nowocien <dominik.nowocien@gmail.com>
2020-01-18 14:21:22 +0100

    poprawa bledów w bibliotece cpp

--- Zrodla/DLL_C/C_DLL/c_dll.cpp
+++ Zrodla/DLL_C/C_DLL/c_dll.cpp
@@ -22,8 +22,8 @@ void C_DLL::SingleColorChannel_Red_CPP(unsigned char * r, unsigned char * g, uns
 	__m128i* g_sse = (__m128i*)g;
 	__m128i* b_sse = (__m128i*)b;
 
-	_mm_store_si128(g_sse, *r_sse);
-	_mm_store_si128(b_sse, *r_sse);
+	_mm_storeu_si128(g_sse, *r_sse);
+	_mm_storeu_si128(b_sse, *r_sse);
 }
 
 void C_DLL::SingleColorChannel_Green_CPP(unsigned char * r, unsigned char * g, unsigned char * b)
@@ -32,8 +32,8 @@ void C_DLL::SingleColorChannel_Green_CPP(unsigned char * r, unsigned char * g, u
 	__m128i* g_sse = (__m128i*)g;
 	__m128i* b_sse = (__m128i*)b;
 
-	_mm_store_si128(r_sse, *g_sse);
-	_mm_store_si128(b_sse, *g_sse);
+	_mm_storeu_si128(r_sse, *g_sse);
+	_mm_storeu_si128(b_sse, *g_sse);
 }
 
 void C_DLL::SingleColorChannel_Blue_CPP(unsigned char* r, unsigned char* g, unsigned char* b)
@@ -42,8 +42,8 @@ void C_DLL::SingleColorChannel_Blue_CPP(unsigned char* r, unsigned char* g, unsi
 	__m128i* g_sse = (__m128i*)g;
 	__m128i* b_sse = (__m128i*)b;
 
-	_mm_store_si128(g_sse, *b_sse);
-	_mm_store_si128(r_sse, *b_sse);
+	_mm_storeu_si128(g_sse, *b_sse);
+	_mm_storeu_si128(r_sse, *b_sse);
 }
 
 void C_DLL::Decomposition_max_CPP(unsigned char* r, unsigned char* g, unsigned char* b)
@@ -52,9 +52,9 @@ void C_DLL::Decomposition_max_CPP(unsigned char* r, unsigned char* g, unsigned c
 	__m128i* g_sse = (__m128i*)g;
 	__m128i* b_sse = (__m128i*)b;
 
-	_mm_store_si128(r_sse, _mm_max_epu8(_mm_loadu_si128(r_sse), _mm_max_epu8(_mm_loadu_si128(g_sse), _mm_loadu_si128(b_sse))));
-	_mm_store_si128(b_sse, *r_sse);
-	_mm_store_si128(g_sse, *r_sse);
+	_mm_storeu_si128(r_sse, _mm_max_epu8(_mm_loadu_si128(r_sse), _mm_max_epu8(_mm_loadu_si128(g_sse), _mm_loadu_si128(b_sse))));
+	_mm_storeu_si128(b_sse, *r_sse);
+	_mm_storeu_si128(g_sse, *r_sse);
 }
 
 void C_DLL::Decomposition_min_CPP(unsigned char * r, unsigned char * g, unsigned char * b)
@@ -63,9 +63,9 @@ void C_DLL::Decomposition_min_CPP(unsigned char * r, unsigned char * g, unsigned
 	__m128i* g_sse = (__m128i*)g;
 	__m128i* b_sse = (__m128i*)b;
 
-	_mm_store_si128(r_sse, _mm_min_epi8(_mm_loadu_si128(r_sse), _mm_min_epi8(_mm_loadu_si128(g_sse), _mm_loadu_si128(b_sse))));
-	_mm_store_si128(b_sse, *r_sse);
-	_mm_store_si128(g_sse, *r_sse);
+	_mm_storeu_si128(r_sse, _mm_min_epu8(_mm_loadu_si128(r_sse), _mm_min_epu8(_mm_loadu_si128(g_sse), _mm_loadu_si128(b_sse))));
+	_mm_storeu_si128(b_sse, *r_sse);
+	_mm_storeu_si128(g_sse, *r_sse);
 }
 
 void C_DLL::Desaturation_CPP(unsigned char * r, unsigned char * g, unsigned char * b)
@@ -73,18 +73,17 @@ void C_DLL::Desaturation_CPP(unsigned char * r, unsigned char * g, unsigned char
 	__m128i* r_sse = (__m128i*)r;
 	__m128i* g_sse = (__m128i*)g;
 	__m128i* b_sse = (__m128i*)b;
+	
+	__m128i min;
+	__m128i max;
 
-	__m128i* min = r_sse;
-	__m128i* max = r_sse;
+	_mm_storeu_si128(&min, _mm_min_epu8(_mm_loadu_si128(r_sse), _mm_min_epu8(_mm_loadu_si128(g_sse), _mm_loadu_si128(b_sse))));
+	_mm_storeu_si128(&max, _mm_max_epu8(_mm_loadu_si128(r_sse), _mm_max_epu8(_mm_loadu_si128(g_sse), _mm_loadu_si128(b_sse))));
+	 
+	_mm_storeu_si128(&min, _mm_srli_epi16(min, 1));
+	_mm_storeu_si128(&max, _mm_srli_epi16(max, 1));
 
-	_mm_store_si128(min, _mm_min_epu8(_mm_loadu_si128(r_sse), _mm_min_epu8(_mm_loadu_si128(g_sse), _mm_loadu_si128(b_sse))));
-	_mm_store_si128(max, _mm_max_epu16(_mm_loadu_si128(r_sse), _mm_max_epu16(_mm_loadu_si128(g_sse), _mm_loadu_si128(b_sse))));
-
-	_mm_srli_epi16(*min, 1);
-	_mm_srli_epi16(*max, 1);
-
-	_mm_store_si128(r_sse, _mm_adds_epu8(*min, *max));
-	_mm_store_si128(b_sse, *r_sse);
-	_mm_store_si128(g_sse, *r_sse);
-
+	_mm_storeu_si128(r_sse, _mm_adds_epu8(min,max));
+	_mm_storeu_si128(b_sse, *r_sse);
+	_mm_storeu_si128(g_sse, *r_sse);
 }
